a0cc7deba28ed8f554e0f68c88c6995ff7aeb507
Murthy <nrshmurthy@gmail.com>
2019-06-10 11:34:57 +0200

    more on book details

--- client/src/components/BoookDetails.jsx
+++ client/src/components/BoookDetails.jsx
@@ -10,8 +10,8 @@ class BookDetails extends Component {
         <div>
           <h2>{book.name}</h2>
           <p>{book.genre}</p>
-          <p>{book.author.name}</p>
-          <p>All books</p>
+          <p>Author: {book.author.name}</p>
+          <p>More books by {book.author.name}</p>
           <ul className="other-books">
             {book.author.books.map(item => {
               return <li key={item.id}>{item.name}</li>;
